11efb8978361dda1e83a43444d323d6b5501516d
arybin <arybin@163.com>
2016-08-05 11:00:24 +0800

    built project structure

---
+++ app/src/main/java/cn/arybin/fearnotwords/activities/BaseActivity.java
@@ -0,0 +1,10 @@
+package cn.arybin.fearnotwords.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.arybin.fearnotwords.R;
+
+public abstract class BaseActivity extends AppCompatActivity {
+ 
+}
